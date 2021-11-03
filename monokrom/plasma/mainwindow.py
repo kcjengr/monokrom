ccdcada690f32254d36ee024d952afc290a5d2ee
James Walker <james.walker.nz@me.com>
2021-11-03 23:44:03 +1300

    Mapping data into UI from ORM

--- monokrom/plasma/mainwindow.py
+++ monokrom/plasma/mainwindow.py
@@ -1,3 +1,4 @@
+from qtpy.QtCore import Qt
 from qtpyvcp.widgets.form_widgets.main_window import VCPMainWindow
 from qtpyvcp.plugins import getPlugin
 
@@ -7,40 +8,59 @@ LOG = logger.getLogger('qtpyvcp.' + __name__)
 
 class MainWindow(VCPMainWindow):
     """Main window class for the VCP."""
+
+    # field map of <plugin data getter>:<ui obj name>
+    fldmap = {
+        'gases':'filter_gas',
+        'machines':'filter_machine',
+        'materials':'filter_material',
+        'thicknesses':'filter_thickness',
+        'linearsystems':'filter_distance_system',
+        'pressuresystems':'filter_pressure_system',
+        'operations':'filter_operation',
+        'qualities':'filter_quality',
+        'consumables':'filter_consumable'
+        }
+    
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self._plasma_plugin = getPlugin('plasmaprocesses')
         
-        # prepare widget data
-        self.load_plasma_data()
+        # prepare widget filter data
+        self.load_plasma_ui_filter_data()
+        
+        # create filter signals
+        for val in MainWindow.fldmap.values():
+            filter_widget = getattr(self, val)
+            filter_widget.currentIndexChanged.connect(self.param_update_from_filters)
         
     # add any custom methods here
 
-    def load_plasma_data(self):
-        fldmap = {
-            'gases':'filter_gas',
-            'machines':'filter_machine',
-            'materials':'filter_material',
-            'thicknesses':'filter_thickness',
-            'linearsystems':'filter_distance_system',
-            'pressuresystems':'filter_pressure_system',
-            'operations':'filter_operation',
-            'qualities':'filter_quality',
-            'consumables':'filter_consumable'
-            }
+    def load_plasma_ui_filter_data(self):
         
         # build up the starting position data for process filters
         # in the UI
-        for k in fldmap:
-            # get filter data and set
+        for k in MainWindow.fldmap:
+            # get filter data and set to self
             setattr(self, '_'+k, getattr(self._plasma_plugin, k)())
             # with this key populate the data into UI fields
-            ui_fld = getattr(self, fldmap[k])
+            ui_fld = getattr(self, MainWindow.fldmap[k])
             # clear down this combo list before adding starting data
             ui_fld.clear()
             for data in getattr(self, '_'+k):
-                ui_fld.addItem(data.name)
-        
+                # add the str name, and the ID as part of user_role data
+                ui_fld.addItem(data.name, data.id)
+
+    # Filter content has changed
+    def param_update_from_filters(self, index):
+        sender = self.sender()
+        print("Update params '{}' '{}'".format(index, sender.currentText()))
+        arglist = []
+        for v in MainWindow.fldmap.values():
+            uifld = getattr(self, v)
+            arglist.append(uifld.currentData())
+        data = self._plasma_plugin.cut(arglist)[0]
+    
     
     def setMode(self):
         print("main window initalise")
