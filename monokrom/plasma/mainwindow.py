3dc2dc7471f79718539b16a0803835562babe8c7
James Walker <james.walker.nz@me.com>
2021-11-05 09:15:36 +1300

    Further expansion of DB to UI linkage

--- monokrom/plasma/mainwindow.py
+++ monokrom/plasma/mainwindow.py
@@ -1,4 +1,5 @@
 from qtpy.QtCore import Qt
+from qtpy.QtWidgets import QLabel
 from qtpyvcp.widgets.form_widgets.main_window import VCPMainWindow
 from qtpyvcp.plugins import getPlugin
 
@@ -10,7 +11,7 @@ class MainWindow(VCPMainWindow):
     """Main window class for the VCP."""
 
     # field map of <plugin data getter>:<ui obj name>
-    fldmap = {
+    filter_fld_map = {
         'gases':'filter_gas',
         'machines':'filter_machine',
         'materials':'filter_material',
@@ -22,6 +23,21 @@ class MainWindow(VCPMainWindow):
         'consumables':'filter_consumable'
         }
     
+    param_fld_map = {
+        'name':'param_name',
+        'pierce_height':'param_pierceheight',
+        'pierce_delay':'param_piercedelay',
+        'cut_height':'param_cutheight',
+        'cut_speed':'param_cutfeedrate',
+        'volts':'param_cutvolts',
+        'kerf_width':'param_kirfwidth',
+        'puddle_height':'param_puddlejumpheight',
+        'puddle_delay':'param_puddlejumpdelay',
+        'amps':'param_cutamps',
+        'pause_at_end':'param_pauseatend',
+        'pressure':'param_gaspressure'
+        }
+    
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self._plasma_plugin = getPlugin('plasmaprocesses')
@@ -30,21 +46,18 @@ class MainWindow(VCPMainWindow):
         self.load_plasma_ui_filter_data()
         
         # create filter signals
-        for val in MainWindow.fldmap.values():
+        for val in MainWindow.filter_fld_map.values():
             filter_widget = getattr(self, val)
             filter_widget.currentIndexChanged.connect(self.param_update_from_filters)
-        
-    # add any custom methods here
 
     def load_plasma_ui_filter_data(self):
-        
         # build up the starting position data for process filters
         # in the UI
-        for k in MainWindow.fldmap:
+        for k in MainWindow.filter_fld_map:
             # get filter data and set to self
             setattr(self, '_'+k, getattr(self._plasma_plugin, k)())
-            # with this key populate the data into UI fields
-            ui_fld = getattr(self, MainWindow.fldmap[k])
+            # with this key populate the data into UI field
+            ui_fld = getattr(self, MainWindow.filter_fld_map[k])
             # clear down this combo list before adding starting data
             ui_fld.clear()
             for data in getattr(self, '_'+k):
@@ -54,12 +67,30 @@ class MainWindow(VCPMainWindow):
     # Filter content has changed
     def param_update_from_filters(self, index):
         sender = self.sender()
-        print("Update params '{}' '{}'".format(index, sender.currentText()))
+        LOG.debug("Update params '{}' '{}'".format(index, sender.currentText()))
         arglist = []
-        for v in MainWindow.fldmap.values():
+        for v in MainWindow.filter_fld_map.values():
             uifld = getattr(self, v)
             arglist.append(uifld.currentData())
-        data = self._plasma_plugin.cut(arglist)[0]
+        cutlist = self._plasma_plugin.cut(arglist)
+        if len(cutlist) == 1:
+            data = cutlist[0]
+            for k in MainWindow.param_fld_map:
+                fld_data = getattr(data, k)
+                ui_fld = getattr(self, MainWindow.param_fld_map[k])
+                if isinstance(ui_fld, QLabel):
+                    ui_fld.setText(fld_data)
+                else:
+                    ui_fld.setValue(fld_data) 
+        else:
+            data = None
+            # set cut params to 0
+            ui_fld = getattr(self, 'param_name')
+            ui_fld.setText('NONE')
+            for v in MainWindow.param_fld_map.values():
+                if v != 'param_name':
+                    ui_fld = getattr(self, v)
+                    ui_fld.setValue(0)
     
     
     def setMode(self):
